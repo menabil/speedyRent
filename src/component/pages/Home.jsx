000b21482212bea66b3f7dfb42df7075f7acfb52
menabil <nabil99cc@gmail.com>
2026-02-26 22:25:36 +0600

    add home page

---
+++ src/component/pages/Home.jsx
@@ -0,0 +1,23 @@
+import About from "../layouts/About";
+import Banner from "../layouts/Banner";
+import Blog from "../layouts/Blog";
+import Experience from "../layouts/Experience";
+import Review from "../layouts/Review";
+import Service from "../layouts/Service";
+import Vehicles from "../layouts/Vehicles";
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <About />
+      <Service />
+      <Experience />
+      <Vehicles />
+      <Review />
+      <Blog />
+    </>
+  );
+};
+
+export default Home;
